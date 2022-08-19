6e9a6400cfee6394e59c1ba3980241c1e6eb03c7
Ben Lee <ben@aircooledcafe.com>
2022-08-19 19:25:07 +0100

    ammended script to ask for ip increment to make automation easier

--- wg_useradd.sh
+++ wg_useradd.sh
@@ -3,8 +3,12 @@ echo "This script needs to run with root privelages to have access to the wiregu
 echo "Please ensure you run this script with 'sudo'."
 echo "This script relies on qrencode to produce a QR code at the end."
 echo "Please provide a name for the new profile:"
-
 read name
+##################################################################
+# up to a valid 8 bit integer for the final IP address increment #
+##################################################################
+echo "What is the IP address increment:"
+read ipincrement
 
 cd /etc/wireguard
 pwd
@@ -33,11 +37,11 @@ echo "PresharedKey = $(cat "${name}.psk")" >> /etc/wireguard/wg0.conf
 ##############################################################
 # INCREMENT YOUR IP ADDRESSESS AS APPROPRIATE TO YOUR CONFIG #
 ##############################################################
-echo "AllowedIPs = 10.100.0.2/32, fd08:4711::2/128" >> /etc/wireguard/wg0.conf
+echo "AllowedIPs = 10.100.0.${ipincrement}/32, fd08:4711::${ipincrement}/128" >> /etc/wireguard/wg0.conf
 
 # create a client config file
 echo "[Interface]" > "${name}.conf"
-echo "Address = 10.100.0.2/32, fd08:4711::2/128" >> "${name}.conf"
+echo "Address = 10.100.0.${ipincrement}/32, fd08:4711::${ipincrement}/128" >> "${name}.conf"
 ####################################################
 # Replace DNS_SERVER with your DNS Servers address #
 ####################################################
